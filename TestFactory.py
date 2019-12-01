09f2b3618e37d626d24e6d2f6f58c19527148f31
Pablo Gadino <pablo.gadino@gmail.com>
2019-11-30 21:45:42 -0300

    'factory cuénta diferencia de días'

--- TestFactory.py
+++ TestFactory.py
@@ -8,10 +8,11 @@ class TestFactory(unittest.TestCase):
     def test_creaAyer(self):
         f = Factory('02/1/2019')
         self.assertIsInstance(f,Factory)
-        print (f.fecha.date())
-        print (f.ahora.date())
 
-
+    def test_creaHoy(self):
+        f = Factory('29/11/2019')
+        print (f.hace_cuántos_días())
+        print (f.hoy)
 
 if __name__ == '__main__':
     unittest.main()
--- factory.py
+++ factory.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from Ayer import Ayer
+from Hoy import Hoy
 
 
 class Factory:
 
     def __init__(self, str_fecha):
         self.str_fecha = str_fecha
-        self.fecha = datetime.strptime(str_fecha, '%d/%m/%Y').date()
+        self.fecha_y_hora_entrega = datetime.strptime(str_fecha, '%d/%m/%Y')
+        self.fecha_entrega=self.fecha_y_hora_entrega.date()
 
         self.ahora = datetime.now()
         self.str_ahora = "% s / % s / % s " % (self.ahora.day, self.ahora.month, self.ahora.year)
@@ -16,4 +18,11 @@ class Factory:
     def generar(self):
         if(self.fecha==self.hoy):
             return Hoy(self.fecha)
+        if(self.hace_cuántos_días()==1)
+            return Ayer(self.fecha)
+        if (self.hace_cuántos_días() == 1)
+            return Ayer(self.fecha)
 
+    def hace_cuántos_días(self):
+        delta = self.fecha_entrega - self.hoy
+        return (delta.days)
